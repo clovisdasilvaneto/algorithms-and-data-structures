e0197bddd311f5225f653b9e5d45b83d6d268d93
Clóvis Neto <clovisdasilvaneto@gmail.com>
2023-02-03 16:20:57 +0100

    feat: solve the counter of number of nice subarrays problem

---
+++ arrays/count-number-of-nice-subarrays.js
@@ -0,0 +1,58 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var numberOfSubarrays = function(nums, k) {
+    let [left, right, total] = [0, 0, 0];
+    let kSize = k;
+    let isForward = false;
+    let leftCounter = 0;
+    let rightCounter = 0;
+
+    while(right < nums.length) {
+        if(nums[right] % 2 !== 0) {
+            kSize--;
+
+            if(kSize === 0) {
+                isForward = true;
+            }
+        }
+
+        //forward left
+        while(kSize === 0) {
+            if(nums[left] % 2 !== 0) {
+                kSize++;
+            }
+
+            leftCounter++;
+            left++;
+        }
+        
+        // //forward right
+        while(isForward) {
+            rightCounter++;
+
+            let currentNum = nums[right + rightCounter];
+            
+            if(currentNum % 2 !== 0) {
+                isForward = false;
+            }
+        }
+
+        total += rightCounter * leftCounter;
+        
+        leftCounter = 0;
+        rightCounter = 0
+
+        right++;
+    }
+
+    
+    return total;
+};
+
+const nums = [2,2,2,1,2,2,1,2,2,2]
+const k = 2;
+
+console.log(`The amount of nice arrays are: ${numberOfSubarrays(nums, k)}`)
