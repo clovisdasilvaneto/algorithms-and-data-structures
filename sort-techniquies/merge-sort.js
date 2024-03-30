fcec37f72439965826864cdc0bff05fd20e6f080
Clovis Neto <clovisdasilvaneto@gmail.com>
2024-03-31 02:48:23 +0900

    feat: create merge sort without sintaxe sugar

--- sort-techniquies/merge-sort.js
+++ sort-techniquies/merge-sort.js
@@ -1,13 +1,35 @@
 const mergeSort = (arr) => {
-    if(arr.length <= 1) return arr;
+    if(arr.length === 1) return arr;
 
-    let middleIndex = Math.round(arr.length / 2);
-    let leftArr = arr.splice(0, middleIndex)
-    let leftSingle = mergeSort(leftArr);
-    let rightSingle = mergeSort(arr);
+    const chunks = Math.floor(arr.length  / 2)
+    const arrOne = arr.slice(0, chunks)
+    const arrTwo = arr.slice(chunks, arr.length)
 
-    return [...leftSingle, ...rightSingle].sort();
+    const left = mergeSort(arrOne);
+    const right = mergeSort(arrTwo);
+
+    if(left.length && right.length) {
+        const sort = []
+        
+        let i = j = 0;
+
+        while(sort.length < left.length + right.length) {
+            if(i >= left.length || left[i] > right[j]) {
+                sort.push(right[j])
+                j++;
+            }else {
+                sort.push(left[i])
+                i++;
+            }
+        }
+        return sort;
+    }else if(left.length) {
+        return left
+    }else {
+        return right
+    }
 }
 
-const input = [5,3,2,1,6,4,7]
-console.log(mergeSort(input))
+const temp = [7,4,10,8,5,7,3]
+
+console.log(mergeSort(temp))
