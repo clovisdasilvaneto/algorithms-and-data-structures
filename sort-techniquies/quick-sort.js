6e40094fd81e26babbb46ba274f81fce2a19b46b
Clovis Neto <clovisdasilvaneto@gmail.com>
2024-03-31 02:22:04 +0900

    feat: add quick sort algorithm

---
+++ sort-techniquies/quick-sort.js
@@ -0,0 +1,28 @@
+const quickSort = (arr) => {
+    if(arr.length <= 1) return arr
+
+    const pivot = arr.length - 1;
+    const left = [], right = []
+
+    for(let i = 0; i < arr.length; i++) {
+        if(i === pivot) continue;
+        if(arr[i] <= arr[pivot]) {
+            left.push(arr[i])
+        }else {
+            right.push(arr[i])
+        }
+    }
+
+    let sort = []
+    if(left.length) sort = [...quickSort(left)]
+    
+    sort.push(arr[pivot])
+    
+    if(right.length) sort = [...sort, ...quickSort(right)]
+
+    return sort
+}
+
+const arr = [7,8,7,4,10,3,5]
+
+console.log(quickSort(arr))
